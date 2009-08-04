ec1a8d24bf0f860b5015f275c874c824155d7c23
Miguel Cabrera <mfcabrera@gmail.com>
2009-08-03 23:27:09 -0500

    testing out some views, next time remeber to create a branch :P

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -2,7 +2,26 @@ class ForecastController < ApplicationController
   
 
   def view
-    @dates = Forecast.find_by_sql("select distinct forecast_date from forecasts")
+    @dates = Forecast.find_by_sql("select distinct forecast_date from  forecasts")
+    @dates_by_day =  {}
+    @dates.sort! {|a,b| 
+          
+      # puts "#{a.forecast_date} - #{b.forecast_date}"
+      a.forecast_date <=> b.forecast_date 
+     
+      
+      
+      }
+    
+    @dates.each { |date|
+      formated_date = date.forecast_date.strftime("%Y-%m-%d %Hh")
+      puts formated_date
+      if @dates_by_day[formated_date].nil?
+        @dates_by_day[formated_date] = Array.new
+      end
+      @dates_by_day[formated_date] << date.forecast_date.strftime("%Hh") 
+    }
+    @keys = @dates_by_day.keys.sort
     @all = Forecast.find(:all)
     @variables = Variable.find(:all)
   end
