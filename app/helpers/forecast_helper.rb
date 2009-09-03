06dbe93599b93a31204566690134c599810449e3
Miguel Cabrera <mfcabrera@gmail.com>
2009-09-03 00:33:58 -0500

    modified table layout  and refactored view based on partials. added more data from the forecast and started generating the feature of local user time for forecasts.

--- app/helpers/forecast_helper.rb
+++ app/helpers/forecast_helper.rb
@@ -3,5 +3,8 @@ module ForecastHelper
 #     var = Variable.find_by_name(var_name)
 #     name = var.nil? ? var_name  : var.human_name    
 #   end
+  def partial_for_var(var_name)
+    "variables/simple_float.html.rb"
+  end
   
 end
