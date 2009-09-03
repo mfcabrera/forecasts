df47d8b12d2f7d81e9dc3e6ec23d38c7728001b8
Miguel Cabrera <mfcabrera@gmail.com>
2009-09-03 00:32:34 -0500

    modified table layout  and refactored view based on partials. added more data from the forecast and started generating the feature of local user time for forecasts.

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,13 +1,29 @@
 class ForecastController < ApplicationController
   
+
+
   layout 'standard'
   def view
+    
+    @date_format = "%a %d"
+    @time_format = "%Hh."
+    @f_offset = 3 #when are we going to start showing the forecasts?
+    @max_per_row = 31 #how many per row
+    
     
     float_regex =  /^-?[0-9]+\.?[0-9]*$/
     
     @lat = params[:lat]
     @lon = params[:lon]
+
     
+    @fpoint = ForecastPoint.find_by_lat_and_lon(@lat,@lon)
+        
+    unless @fpoint
+      render :nothing => true
+    end
+
+
     unless ( @lat =~ float_regex and  @lon =~ float_regex )
       #TODO: Error pages when the parameters are malformed
       render :nothing => true            
@@ -15,9 +31,12 @@ class ForecastController < ApplicationController
       @all = Forecast.find_all_by_lat_and_lon(@lat,@lon)
       if @all.size == 0
         #TODO: put an empty message
-        render :nothing => true
+        render :nothing => true        
       end
     end
+
+    #get forecast metadata
+    @meta = GribMeta.find(:all)[0]
 
     
     @dates = Forecast.find_by_sql("select distinct forecast_date from  forecasts")
@@ -38,6 +57,7 @@ class ForecastController < ApplicationController
     }
     @keys = @dates_by_day.keys.sort
     @variables = Variable.find(:all)
+    @max_per_row = @f_offset + @dates.size / 2                  
     
   end
 
